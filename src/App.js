84998d956a7e0c703cc67808eb72aafdee014ce5
max <max.aleksandrovich163@gmail.com>
2021-03-03 15:13:50 +0400

    deploy test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Test from './Test';
 
 function App() {
 	return (
 		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-				<p>Deploy test</p>
-			</header>
+			<h1>Github deploy to firebase test</h1>
+			<Test />
 		</div>
 	);
 }
