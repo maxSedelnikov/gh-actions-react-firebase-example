1adbddad8ba68445882b810149e78731b82f4fc0
max <max.aleksandrovich163@gmail.com>
2021-03-03 15:25:16 +0400

    change tabs to spaces

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import Test from './Test';
 
 function App() {
-	return (
-		<div className='App'>
-			<h1>Github deploy to firebase test</h1>
-			<Test />
-		</div>
-	);
+  return (
+    <div className='App'>
+      <h1>Github deploy to firebase test</h1>
+      <Test />
+    </div>
+  );
 }
 
 export default App;
--- src/Test.js
+++ src/Test.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
 const Test = (props) => {
-	return (
-		<div>
-			<ul>
-				<li>
-					Deploy code to github <span>&#9989;</span>
-				</li>
-				<li>
-					Deliver code to firebase hosting <span>&#9989;</span>
-				</li>
-			</ul>
-		</div>
-	);
+  return (
+    <div>
+      <ul>
+        <li>
+          Deploy code to github <span>&#9989;</span>
+        </li>
+        <li>
+          Deliver code to firebase hosting <span>&#9989;</span>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Test;
