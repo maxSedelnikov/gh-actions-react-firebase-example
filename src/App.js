8d98f834825adea33fe45e2330f9c8d03aba1d92
max <max.aleksandrovich163@gmail.com>
2021-03-30 15:15:56 +0400

    adding docs

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import React from 'react';
 import './App.css';
 import Test from './Test';
 
-function App() {
+/**
+ * App starter component
+ *
+ * @component
+ *
+ * @returns {JSX} <App />
+ * 
+ * @example
+ * 
+ * return (
+    <div className='App'>
+      <h1>Github deploy to firebase test</h1>
+      <Test />
+    </div>
+  );
+ * 
+ */
+
+const App = () => {
   return (
     <div className='App'>
       <h1>Github deploy to firebase test</h1>
       <Test />
     </div>
   );
-}
+};
 
 export default App;
